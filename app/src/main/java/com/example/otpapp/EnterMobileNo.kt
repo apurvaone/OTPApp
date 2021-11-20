692d5d347b505483cfffe24526f2d2da2d6d2854
apurvaone <57245377+apurvaone@users.noreply.github.com>
2021-11-20 11:00:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/otpapp/EnterMobileNo.kt
@@ -0,0 +1,11 @@
+package com.example.otpapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EnterMobileNo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_entermobileno)
+    }
+}
